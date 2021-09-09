4687dfe2ab4321e36c1b44f67e906058921d341a
Nicolle Montilla R <nicolle.montilla@gmail.com>
2021-09-08 21:22:23 -0500

    finish the slidebar with corrections

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const barsBtn = document.querySelector('.header__hamburguer');
+const hamburguerBtn = document.querySelector('.header__hamburguer');
 const slideBar = document.querySelector('.header__nav');
 
-barsBtn.addEventListener('click',() => {
+hamburguerBtn.addEventListener('click',() => {
     if (slideBar.classList.contains('show-slidebar')) {
         slideBar.classList.remove('show-slidebar');
-    }else {
-        slideBar.classList.add ('show-slidebar');
+    } else {
+        slideBar.classList.add('show-slidebar');
     };
 });
